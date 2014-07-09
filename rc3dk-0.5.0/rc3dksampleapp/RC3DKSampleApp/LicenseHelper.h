ed83c68ea92d2e18a2b6ef76900b57ad0ad93cd8
Patrickuang <patrickuang@gmail.com>
2014-07-09 15:47:47 +0800

    add ignore file

--- rc3dk-0.5.0/rc3dksampleapp/RC3DKSampleApp/LicenseHelper.h
+++ rc3dk-0.5.0/rc3dksampleapp/RC3DKSampleApp/LicenseHelper.h
@@ -11,6 +11,7 @@
 #error You must insert your 3DK license key here and delete this line
 #define SDK_LICENSE_KEY @"YOUR_LICENSE_KEY_HERE"
 
+
 @interface LicenseHelper : NSObject
 
 + (void)showLicenseValidationError:(NSError *)error;
